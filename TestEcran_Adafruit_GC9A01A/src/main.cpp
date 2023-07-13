3735fb0d6659a2f531272ff18256b0a5f6bcbd4c
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-07-13 17:02:16 -0400

    Écran fonctionnel avec Lilygo en VSPI

--- TestEcran_Adafruit_GC9A01A/src/main.cpp
+++ TestEcran_Adafruit_GC9A01A/src/main.cpp
@@ -21,14 +21,23 @@
 
     #else   // VSPI
 
-    #define TFT_BL_BLK   39     // LED back-light
+    #define TFT_BL_BLK   -1 //39     // LED back-light
     #define TFT_CS   05 //25         // Chip select control pin
-    #define TFT_DC   19 //36         // Data Command control pin
-    #define TFT_RES_RST  00     // Reset pin (could connect to Arduino RESET pin)
+    #define TFT_DC   00 //19 //36         // Data Command control pin
+    #define TFT_RES_RST  -1     // Reset pin (could connect to Arduino RESET pin)
     #define TFT_SDA_DIN_MOSI 23 // In some display driver board, it might be written as "SDA" and so on.
     #define TFT_SCL_CLK_SCK 18  // In some display driver board, it might be written as "SCL" and so on.
     // #define TFT_MISO 19      // Ne semble pas être utile
 
+/*!
+    @brief  Instantiate Adafruit GC9A01A driver with software SPI
+    @param  cs    Chip select pin #   05
+    @param  dc    Data/Command pin #    19
+    @param  mosi  SPI MOSI pin #    23
+    @param  sclk  SPI Clock pin #   18
+    @param  rst   Reset pin # (optional, pass -1 if unused)
+    @param  miso  SPI MISO pin # (optional, pass -1 if unused)
+*/
     #endif
 
 #endif
