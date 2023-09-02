d00351b5beea165e535502fe08cf310ca6211d40
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-09-01 20:56:21 -0400

    Créer la méthode principale Execute() du Program

--- src/AppEmbarqueeTS/include/Program.h
+++ src/AppEmbarqueeTS/include/Program.h
@@ -1,13 +1,36 @@
 #pragma once
 #include <Arduino.h>
-#include "Interfaces/ILTE.h"
+#include "Configurations.h"
+#include "TrackSenseProperties.h"
+#include "ControlerButtons.h"
+
+#include "Modules/LTE.h"
+#include "Modules/GPSTinyPlus.h"
+#include "Modules/SDCard.h"
+#include "Modules/GyroscopeMPU6050.h"
+#include "Modules/CompassHMC5883L.h"
+#include "Modules/AccelerometerMPU6050.h"
+#include "Modules/BLE.h"
+#include "Modules/ScreenGC9A01.h"
+#include "Modules/Buzzer.h"
+
 
 
 
 class Program
 {
 private:
-    ILTE *lte;
+    TrackSenseProperties *_trackSenseProperties;
+    LTE *_lte;
+    GPSTinyPlus *_gps;
+    SDCard *_sdCard;
+    GyroscopeMPU6050 *_gyroscope;
+    CompassHMC5883L *_compass;
+    AccelerometerMPU6050 *_accelerometer;
+    BLE *_ble;
+    ScreenGC9A01 *_screen;
+    Buzzer *_buzzer;
+    ControlerButtons *_controlerButtons;
 
 public:
     Program();
--- src/AppEmbarqueeTS/src/Program.cpp
+++ src/AppEmbarqueeTS/src/Program.cpp
@@ -2,8 +2,30 @@
 
 
 
-Program::Program()
+Program::Program() : 
+    _trackSenseProperties(nullptr), 
+    _lte(nullptr), 
+    _gps(nullptr), 
+    _sdCard(nullptr), 
+    _gyroscope(nullptr), 
+    _compass(nullptr), 
+    _accelerometer(nullptr), 
+    _ble(nullptr), 
+    _screen(nullptr), 
+    _buzzer(nullptr)
 {
+    this->_trackSenseProperties = new TrackSenseProperties();
+    this->_controlerButtons = new ControlerButtons(this->_trackSenseProperties);
+    this->_screen = new ScreenGC9A01(this->_trackSenseProperties);
+    this->_ble = new BLE(this->_trackSenseProperties);
+    this->_sdCard = new SDCard(this->_trackSenseProperties);
+    this->_gps = new GPSTinyPlus(this->_trackSenseProperties);
+    this->_lte = new LTE(this->_trackSenseProperties);
+    this->_gyroscope = new GyroscopeMPU6050(this->_trackSenseProperties);
+    this->_compass = new CompassHMC5883L(this->_trackSenseProperties);
+    this->_accelerometer = new AccelerometerMPU6050(this->_trackSenseProperties);
+    this->_buzzer = new Buzzer(this->_trackSenseProperties);
+
 }
 
 Program::~Program()
@@ -12,5 +34,15 @@ Program::~Program()
 
 void Program::execute()
 {
-    Serial.println("Hello World");
+    // Serial.println("Hello World");
+    this->_controlerButtons->tick();
+    this->_screen->tick();
+    this->_ble->tick();
+    this->_sdCard->tick();
+    this->_gps->tick();
+    this->_lte->tick();
+    this->_gyroscope->tick();
+    this->_compass->tick();
+    this->_accelerometer->tick();
+    // this->_buzzer->tick();   // gossant
 }
