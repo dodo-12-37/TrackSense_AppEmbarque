394c4a9806c4fd1e82f27be915ffb8d005531479
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-09-01 20:49:47 -0400

    Ajout méthode Tick() de l'écran avec un exemple

--- src/AppEmbarqueeTS/include/Modules/SDCard.h
+++ src/AppEmbarqueeTS/include/Modules/SDCard.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <Arduino.h>
 #include "Interfaces/ISDCard.h"
+#include "Configurations.h"
+#include "TrackSenseProperties.h"
+#include <SPI.h>
+#include <SD.h>
+
 
 
 
 class SDCard : ISDCard
 {
 private:
-    /* data */
+    TrackSenseProperties* trackSenseProperties;
+    // SD* _sd;
+
 public:
-    SDCard();
+    SDCard(TrackSenseProperties* trackSenseProperties);
     ~SDCard();
+
+    void init() override;
+    void tick() override;
 };
--- src/AppEmbarqueeTS/src/Modules/SDCard.cpp
+++ src/AppEmbarqueeTS/src/Modules/SDCard.cpp
@@ -2,10 +2,55 @@
 
 
 
-SDCard::SDCard()
+SDCard::SDCard(TrackSenseProperties* trackSenseProperties) : trackSenseProperties(trackSenseProperties)
 {
+    this->init();
 }
 
 SDCard::~SDCard()
 {
+}
+
+void SDCard::init()
+{
+    Serial.println("Initializing SD card...");
+    SPI.begin(PIN_SDCARD_SCLK, PIN_SDCARD_MISO, PIN_SDCARD_MOSI);
+
+    if (!SD.begin(PIN_SDCARD_CS))
+    {
+        Serial.println("initialization failed!");
+        return;
+    }
+    Serial.println("initialization done.");
+}
+
+void SDCard::tick()
+{
+    // Serial.println("SDCard");
+    // Serial.println("Writing to test.txt...");
+    // File file = SD.open("/test.txt", FILE_WRITE);
+    // if (file)
+    // {
+    //     file.println("Testing 1, 2, 3.");
+    //     file.close();
+    //     Serial.println("done.");
+    // }
+    // else
+    // {
+    //     Serial.println("error opening test.txt");
+    // }
+    // Serial.println("Reading from test.txt...");
+    // file = SD.open("/test.txt");
+    // if (file)
+    // {
+    //     while (file.available())
+    //     {
+    //         Serial.write(file.read());
+    //     }
+    //     file.close();
+    // }
+    // else
+    // {
+    //     Serial.println("error opening test.txt");
+    // }
 }
