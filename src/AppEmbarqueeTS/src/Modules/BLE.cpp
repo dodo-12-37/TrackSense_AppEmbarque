1f2005ba2d295531e0b8de5debdebc6fddde60aa
simonquillaud <quillaudsimon@outlook.com>
2023-09-04 16:49:06 -0400

    Ajout propriétées UUID pour CompletedRide dans BLE

--- src/AppEmbarqueeTS/src/Modules/BLE.cpp
+++ src/AppEmbarqueeTS/src/Modules/BLE.cpp
@@ -1,5 +1,10 @@
-#include "Modules/BLE.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLESecurity.h>
+#include <BLE2902.h>
 
+#include "Modules/BLE.h"
 
 
 BLE::BLE(TrackSenseProperties* trackSenseProperties) : _trackSenseProperties(trackSenseProperties)
