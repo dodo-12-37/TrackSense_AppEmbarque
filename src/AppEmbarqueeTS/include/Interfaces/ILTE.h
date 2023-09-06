e0b5b9be916466a00f5cb6bed1aed5c7f5b1f322
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-09-06 16:05:00 -0400

    Supprimer/commenter ancien code pour LTE et GPS.

--- src/AppEmbarqueeTS/include/Interfaces/ILTE.h
+++ src/AppEmbarqueeTS/include/Interfaces/ILTE.h
@@ -1,10 +1,11 @@
-#pragma once
-#include "TrackSenseProperties.h"
+/*======================* Est devenu IGSM ======================*/
+// #pragma once
+// #include "TrackSenseProperties.h"
 
 
 
-class ILTE
-{
-public:
-    virtual void tick() = 0;
-};
+// class ILTE
+// {
+// public:
+//     virtual void tick() = 0;
+// };
