8b3b692a15ff4b43dbee1a5b24197ea6269598ba
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-09-06 13:26:03 -0400

    Exemple fonctionnel du GPS !

--- src/Tests/TestGPS_TinyGSM/src/main.cpp
+++ src/Tests/TestGPS_TinyGSM/src/main.cpp
@@ -10,9 +10,10 @@
 */
 #define DISABLE_GPS_AND_MODEM FALSE
 
-void printGPSOnScreen(float lat, float lon, float speed, float alt, float accuracy, int counterTotal, int counterGoodValue);
+void printGPSOnScreen(float lat, float lon, float speed, float alt, float accuracy, int usat, int counterTotal, int counterGoodValue);
 void enableGPSAndModem();
 void disableGPSAndModem();
+void showScreenWaitingPage();
 
 #define TINY_GSM_MODEM_SIM7000
 #define TINY_GSM_RX_BUFFER 1024 // Set RX buffer to 1Kb
@@ -52,26 +53,12 @@ Adafruit_GC9A01A tft(TFT_CS_SS, TFT_DC, TFT_SDA_DIN_MOSI, TFT_SCL_CLK_SCK, TFT_R
 
 void setup()
 {
-    /* Set Screen */
-    tft.begin();
-    tft.fillScreen(GC9A01A_BLACK);
-    tft.setRotation(0);
-    
-    tft.setTextColor(GC9A01A_WHITE, GC9A01A_BLACK);
-    tft.setCursor(30, 40);
-    tft.setTextSize(3);
-    tft.printf("%-11s", "TrackSense");
-    tft.setCursor(10, 150);
-    tft.printf("%-13s", "Initializing");
-    tft.setCursor(90, 180);
-    tft.printf("%-4s", "GPS");
-
     SerialMon.begin(115200);
     SerialMon.println("Place your board outside to catch satelite signal");
-    tft.setTextColor(GC9A01A_DARKGREEN, GC9A01A_YELLOW);
-    tft.setCursor(0, 90);
-    tft.setTextSize(2);
-    tft.printf("%-50s", "Place your board outside to catch satelite signal");
+
+    /* Set Screen */
+    tft.begin();
+    showScreenWaitingPage();
 
     // Set LED OFF
     pinMode(LED_PIN, OUTPUT);
@@ -104,18 +91,27 @@ void setup()
     String modemInfo = modem.getModemInfo();
     delay(500);
     SerialMon.println("Modem Info: " + modemInfo);
-
-    // Init String on Screen
-    tft.fillScreen(GC9A01A_BLACK);
-    printGPSOnScreen(0, 0, 0, 0, 0, -1, -1);
-
-    // Open port 4 for GPS
-    void enableGPSAndModem();
 }
 
 void loop()
 {
+    // Set SIM7000G GPIO4 HIGH ,turn on GPS power
+    // CMD:AT+SGPIO=0,4,1,1
+    // Only in version 20200415 is there a function to control GPS power
+    modem.sendAT("+SGPIO=0,4,1,1");
+    if (modem.waitResponse(10000L) != 1)
+    {
+        SerialMon.println(" SGPIO=0,4,1,1 false ");
+    }
+
+    modem.enableGPS();
+
     delay(15000);
+
+    // Init String on Screen
+    tft.fillScreen(GC9A01A_BLACK);
+    printGPSOnScreen(0, 0, 0, 0, 0, 0, -1, -1);
+
     float lat = 0;
     float lon = 0;
     float speed = 0;
@@ -130,66 +126,48 @@ void loop()
     int min = 0;
     int sec = 0;
 
-    modem.getGsmLocation();
-    SerialMon.println();
-    SerialMon.print("GSM Based Location: ");
-    SerialMon.println(modem.getGsmLocation());
-    SerialMon.println();
-    SerialMon.println();
-
     int counterGoodValue = 0;
+    int counterTotal = 1;
 
-    for (int counterTotal = 0; counterTotal < 1000; counterTotal++)
+    for (int8_t i = 100; i; i--)
     {
+        /* Print on LCD Screen */
+        printGPSOnScreen(lat, lon, speed, alt, accuracy, usat, counterTotal, counterGoodValue);
+
         SerialMon.println("=======================================");
         SerialMon.println("Requesting current GPS/GNSS/GLONASS location");
+
         if (modem.getGPS(&lat, &lon, &speed, &alt, &vsat, &usat, &accuracy,
                          &year, &month, &day, &hour, &min, &sec))
         {
-            SerialMon.println("Latitude: " + String(lat, 8) + "\tLongitude: " + String(lon, 8));
+            SerialMon.println("Latitude: " + String(lat, 10) + "\tLongitude: " + String(lon, 10));
             SerialMon.println("Speed: " + String(speed) + "\tAltitude: " + String(alt));
             SerialMon.println("Visible Satellites: " + String(vsat) + "\tUsed Satellites: " + String(usat));
             SerialMon.println("Accuracy: " + String(accuracy));
             SerialMon.println("Year: " + String(year) + "\tMonth: " + String(month) + "\tDay: " + String(day));
             SerialMon.println("Hour: " + String(hour) + "\tMinute: " + String(min) + "\tSecond: " + String(sec));
+
+            SerialMon.println();
+            SerialMon.println("Retrieving GPS/GNSS/GLONASS location again as a string");
+            String gps_raw = modem.getGPSraw();
+            SerialMon.println("GPS/GNSS Based Location String: " + gps_raw);
+
+            counterGoodValue++;
+
+            SerialMon.println("=======================================");
+            delay(1000L);
         }
         else
         {
-            SerialMon.println("Couldn't get GPS/GNSS/GLONASS location");
+            SerialMon.println("Couldn't get GPS/GNSS/GLONASS location, retrying in 10s.");
+            delay(10000L);
         }
 
-        /* Print on LCD Screen */
-        printGPSOnScreen(lat, lon, speed, alt, accuracy, counterTotal, counterGoodValue);
         counterTotal++;
-
-        SerialMon.println("Retrieving GPS/GNSS/GLONASS location again as a string");
-        String gps_raw = modem.getGPSraw();
-        SerialMon.println("GPS/GNSS Based Location String: " + gps_raw);
-        SerialMon.println("=======================================");
     }
 
-#if DISABLE_GPS_AND_MODEM
-    // Turn off the modem and GPS
-    disableGPSAndModem();
-#endif
-}
+    printGPSOnScreen(lat, lon, speed, alt, accuracy, usat, counterTotal, counterGoodValue);
 
-void enableGPSAndModem()
-{
-    // Set SIM7000G GPIO4 HIGH ,turn on GPS power
-    // CMD:AT+SGPIO=0,4,1,1
-    // Only in version 20200415 is there a function to control GPS power
-    modem.sendAT("+SGPIO=0,4,1,1");
-    if (modem.waitResponse(10000L) != 1)
-    {
-        SerialMon.println(" SGPIO=0,4,1,1 false ");
-    }
-
-    modem.enableGPS();
-}
-
-void disableGPSAndModem()
-{
     SerialMon.println("Disabling GPS");
     modem.disableGPS();
 
@@ -210,9 +188,28 @@ void disableGPSAndModem()
     }
 }
 
-void printGPSOnScreen(float lat, float lon, float speed, float alt, float accuracy, int counterTotal, int counterGoodValue)
+void showScreenWaitingPage()
 {
-    char *formatChar = (char *)"%-29s";
+    tft.fillScreen(GC9A01A_BLACK);
+    tft.setRotation(0);
+    tft.setTextColor(GC9A01A_WHITE, GC9A01A_BLACK);
+    tft.setCursor(30, 40);
+    tft.setTextSize(3);
+    tft.printf("%-11s", "TrackSense");
+    tft.setCursor(10, 150);
+    tft.printf("%-13s", "Initializing");
+    tft.setCursor(90, 180);
+    tft.printf("%-4s", "GPS");
+
+    tft.setTextColor(GC9A01A_DARKGREEN, GC9A01A_YELLOW);
+    tft.setCursor(0, 90);
+    tft.setTextSize(2);
+    tft.printf("%-50s", "Place your board outside to catch satelite signal");
+}
+
+void printGPSOnScreen(float lat, float lon, float speed, float alt, float accuracy, int usat, int counterTotal, int counterGoodValue)
+{
+    char *formatChar = (char *)"%-19s";
     bool locationIsValid = false;
 
     if (lat != 0 && lon != 0)
@@ -220,9 +217,13 @@ void printGPSOnScreen(float lat, float lon, float speed, float alt, float accura
         locationIsValid = true;
     }
 
-    if (locationIsValid)
+    if (locationIsValid && usat >= 4)
     {
         tft.setTextColor(GC9A01A_GREEN, GC9A01A_BLACK);
+    }
+    else if (locationIsValid && usat < 4)
+    {
+        tft.setTextColor(GC9A01A_YELLOW, GC9A01A_BLACK);
     }
     else
     {
@@ -234,24 +235,28 @@ void printGPSOnScreen(float lat, float lon, float speed, float alt, float accura
     tft.setCursor(40, 40);
     String strCounterGoodValue = "Good : " + String(counterGoodValue);
     tft.printf("%-15s", strCounterGoodValue.c_str());
-    
+
     tft.setCursor(30, 60);
     String strCounterTotal = "Total : " + String(counterTotal);
     tft.printf("%-11s", strCounterTotal.c_str());
 
-    tft.setCursor(30, 90);
-    String strIsValid = "IsValid : " + String(locationIsValid ? "true" : "false");
-    tft.printf(formatChar, strIsValid.c_str());
+    // tft.setCursor(30, 90);
+    // String strIsValid = "IsValid : " + String(locationIsValid ? "true" : "false");
+    // tft.printf(formatChar, strIsValid.c_str());
 
-    tft.setCursor(20, 110);
-    String strLatitude = "Lat : " + String(lat, 8);
+    tft.setCursor(15, 85);
+    String strUsedSatellite = "Used Sat : " + String(usat);
+    tft.printf(formatChar, strUsedSatellite.c_str());
+
+    tft.setCursor(2, 110);
+    String strLatitude = "Lat : " + String(lat, 10);
     tft.printf(formatChar, strLatitude.c_str());
 
-    tft.setCursor(20, 130);
-    String strLongitude = "Long : " + String(lon, 8);
+    tft.setCursor(2, 130);
+    String strLongitude = "Lon : " + String(lon, 10);
     tft.printf(formatChar, strLongitude.c_str());
 
-    tft.setCursor(20, 150);
+    tft.setCursor(12, 150);
     String strAltitude = "Alt : " + String(alt, 8);
     tft.printf(formatChar, strAltitude.c_str());
 
@@ -259,8 +264,12 @@ void printGPSOnScreen(float lat, float lon, float speed, float alt, float accura
     String strSpeed = "Speed : " + String(speed, 4);
     tft.printf(formatChar, strSpeed.c_str());
 
-    tft.setCursor(30, 190);
+    tft.setCursor(40, 190);
     String strAccuracy = "Accu : " + String(accuracy, 4);
     tft.printf(formatChar, strAccuracy.c_str());
-
 }
+
+
+// Coordonnées Borne Fontaine : 46.8428955078, -71.4045486450
+// Coordonnées Borne Fontaine : 46.8428916931, -71.4045486450
+// Coordonnées Borne Fontaine : 46.8429985046, -71.4049835205
