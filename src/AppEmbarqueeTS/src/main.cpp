af85314b57c2699c370cb658fd802f0e49f51546
Dominique Lebrun <dodo-12-37@hotmail.com>
2023-10-19 00:54:12 -0400

    formatage texte

--- src/AppEmbarqueeTS/src/main.cpp
+++ src/AppEmbarqueeTS/src/main.cpp
@@ -1,46 +1,44 @@
 #include <Arduino.h>
 #include "Program.h"
 
-
-
 void loopCore0(void *pvParameters); // forward declaration of the loopCore0 function
 
 Program *program = nullptr;
 
 void setup()
 {
-  Serial.end();
-  Serial.begin(115200);
+    Serial.end();
+    Serial.begin(115200);
 
-  program = new Program();
+    program = new Program();
 
-  xTaskCreatePinnedToCore(
+    xTaskCreatePinnedToCore(
         loopCore0,
         "loopCore0", // Name of the task
-        5000,    // Stack size in words // À vérifier !!!
-        NULL,    // Task input parameter
-        100,      // Priority of the task
-        NULL,    // Task handle.
-        0        // Core where the task should run
+        5000,        // Stack size in words // À vérifier !!!
+        NULL,        // Task input parameter
+        100,         // Priority of the task
+        NULL,        // Task handle.
+        0            // Core where the task should run
     );
 }
 
 void loop()
 {
-  program->executeCore1();
-  
-  // delay(1000);
+    program->executeCore1();
+
+    // delay(1000);
 }
 
 void loopCore0(void *pvParameters)
 {
-  while (true)
-  {
-    // Serial.print("          loopCore0() running in core ");
-    // Serial.println(xPortGetCoreID());
+    while (true)
+    {
+        // Serial.print("          loopCore0() running in core ");
+        // Serial.println(xPortGetCoreID());
 
-    program->executeCore0();
-
-    // delay(1000);
-  }
+        program->executeCore0();
+        
+        // delay(1000);
+    }
 }
